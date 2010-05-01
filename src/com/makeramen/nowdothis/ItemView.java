2f9c55635c5344f41be1799f1fcad1619eee3db3
Vincent Mi <vinc3m1@gmail.com>
2010-04-30 19:48:21 -0500

    initial commit

---
+++ src/com/makeramen/nowdothis/ItemView.java
@@ -0,0 +1,13 @@
+package com.makeramen.nowdothis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itemview);
+    }
+}
